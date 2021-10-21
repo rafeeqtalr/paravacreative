03e22c779d617edaf255986996b54def853a4e48
Rafeeq T Alr <rafeeqtalr@gmail.com>
2021-10-21 11:50:56 +0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,116 @@
+
+$( document ).ready(function() {
+    $("#html").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 80,
+            icon: 'f13b',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#css").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 75,
+            icon: 'f13c',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#javascript").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 50,
+            icon: 'f3b9',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#github").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 85,
+            icon: 'f09b',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#bootstrap").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 90,
+            icon: 'f032',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#photoshop").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 70,
+            icon: 'f288',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#illustrator").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 50,
+            icon: 'f129',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#bower").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 90,
+            icon: 'f032',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#04AEA5",
+            fillColor: '#04AEA5',
+            foregroundColor: '#fff',
+            fontColor: '#000',
+            text:"ref"
+    });
+});
---
+++ js/custom_color.js
@@ -0,0 +1,116 @@
+
+$( document ).ready(function() {
+    $("#html").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 80,
+            icon: 'f13b',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#F06529",
+            fillColor: '#F06529',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#css").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 75,
+            icon: 'f13c',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#2965F1",
+            fillColor: '#2965F1',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#javascript").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 50,
+            icon: 'f3b9',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#F1BE21",
+            fillColor: '#F1BE21',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#github").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 85,
+            icon: 'f09b',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#000",
+            fillColor: '#000',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#bootstrap").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 90,
+            icon: 'f032',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#583D80",
+            fillColor: '#583D80',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#photoshop").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 70,
+            icon: 'f288',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#08004B",
+            fillColor: '#08004B',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#illustrator").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 50,
+            icon: 'f129',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#FF7C00",
+            fillColor: '#FF7C00',
+            foregroundColor: '#fff',
+            fontColor: '#000'
+    });
+    $("#bower").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 90,
+            icon: 'f032',
+            iconPosition: 'middle',
+            iconColor: '#fff',
+            iconSize: '40',
+            backgroundColor: "#EF5734",
+            fillColor: '#EF5734',
+            foregroundColor: '#fff',
+            fontColor: '#000',
+            text:"ref"
+    });
+});
